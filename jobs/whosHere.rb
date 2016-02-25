b61802ae84f5ee341c81fe7f6650e9e9e5e09be4
Mike T <miketissenbaum@gmail.com>
2016-02-25 16:11:45 -0600

    Thiings

--- jobs/whosHere.rb
+++ jobs/whosHere.rb
@@ -1,13 +1,17 @@
+# gem "httparty"
 require 'rubygems'
 require 'json'
 require 'pp'
+require 'httparty'
 
 # name_list = Hash.new({ value: 0 })
 event_data = Hash.new({ value: 0 })
 
+
+
 SCHEDULER.every '10s' do
 	locationresponse = HTTParty.post('https://hidden-springs-6751.herokuapp.com/getStatus.json',
-	:body => {"location" => "Room 112"})
+	:body => {"location" => "Room 150"})
 
 	puts locationresponse.body #this must show the JSON contents
 	# json = File.read('/Users/research/inoutdash/sweet_dashboard_project/jobs/list2.json')
